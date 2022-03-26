c4386c63d74ea9d6dab4e5a72b95d835e2fb2447
cahyafaisalreza <cf.reza07@gmail.com>
2022-03-26 17:41:59 +0700

    3 Calculated fields
    - show calculated fields on listview page?

--- myproject/apps/entities/admin.py
+++ myproject/apps/entities/admin.py
@@ -8,6 +8,16 @@ admin.site.unregister(Group)
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Origin)
+# admin.site.register(Origin)
 admin.site.register(Hero)
 admin.site.register(Villain)
+
+@admin.register(Origin)
+class OriginAdmin(admin.ModelAdmin):
+    list_display = ("name", "hero_count", "villain_count")
+
+    def hero_count(self, obj):
+        return obj.hero_set.count()
+
+    def villain_count(self, obj):
+        return obj.villain_set.count()
